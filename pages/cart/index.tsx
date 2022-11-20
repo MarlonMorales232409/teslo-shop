a0de4c4392df441ae3750e69b8bc0b5088da57f0
Marlon Morales <marlon.morales2324@icloud.com>
2022-11-20 12:43:40 +0000

    add: redirect in cart screen to cart empty if cart is empty

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,9 +1,21 @@
 import { Box, Button, Card, CardContent, Divider, Grid, Typography } from '@mui/material';
+import { useRouter } from 'next/router';
+import { useContext } from 'react';
 import { CardList, OrderSumary } from '../../components/cart';
 import { ShopLayout } from '../../components/layout/ShopLayout';
+import { CartContext } from '../../context';
 
 
 const CartPage = () => {
+
+    const {cart} = useContext(CartContext)
+
+    const router = useRouter()
+
+    if(cart.length === 0) {
+        router.push("/cart/empty")
+    }
+
     return (
         <ShopLayout title='Cart - 3' pageDescription='Cart page with 3 items'>
             <Grid container>
