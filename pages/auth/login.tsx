763f4efb18ab79c12feb81c04f420746a835adc7
Marlon Morales <marlon.morales2324@icloud.com>
2022-12-06 14:50:29 +0000

    add: login validation field && add: react hook form

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,23 +1,59 @@
 import { Box, Button, Grid, Link, TextField, Typography } from '@mui/material'
 import NextLink from 'next/link'
+import { useForm } from 'react-hook-form';
 import { AuthLayout } from '../../components/layout'
+import { validation } from '../../utils';
+
+type FormData = {
+    email: string,
+    password: string,
+  };
 
 const login = () => {
+
+    
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+
+    const onLoginUser = (data: FormData)=> {
+        console.log({data})
+    }
+
     return (
         <AuthLayout title="Login">
+            <form onSubmit={ handleSubmit(onLoginUser) } noValidate>
             <Box sx={{ width: 350, padding: "10px 20px" }} >
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
                         <Typography variant='h1' component={"h1"}>Login</Typography>
                     </Grid>
                     <Grid item xs={12}>
-                        <TextField label="E-mail" type="email" fullWidth />
+                        <TextField 
+                            label="E-mail" 
+                            type="email" 
+                            fullWidth 
+                            {...register('email',{
+                                required: 'This field is required',
+                                validate: validation.isEmail,
+                            })}
+                            error={ !!errors.email }
+                            helperText={ errors.email?.message }
+                        />
                     </Grid>
                     <Grid item xs={12}>
-                        <TextField label="Password" type="password" fullWidth />
+                        <TextField 
+                            label="Password" 
+                            type="password" 
+                            fullWidth 
+                            {...register('password',{
+                                required: 'This field is required',
+                                minLength: {value: 6, message: 'Must to have a least 6 characters'}
+                            })}
+                            error={ !!errors.password }
+                            helperText={ errors.password?.message }
+                        />
                     </Grid>
                     <Grid item xs={12}>
-                        <Button className="circular-btn" color="secondary" size='large' fullWidth>Get Into</Button>
+                        <Button type='submit' className="circular-btn" color="secondary" size='large' fullWidth>Login</Button>
                     </Grid>
                     <Grid item xs={12}>
                         <NextLink href={"/auth/register"} passHref>
@@ -33,6 +69,7 @@ const login = () => {
                     </Grid>
                 </Grid>
             </Box>
+            </form>
         </AuthLayout>
     )
 }
